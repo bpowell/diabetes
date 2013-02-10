041af0ca888a020608724ce5caf6c05a4ed496fe
Brandon Powell <bpowell29a@gmail.com>
2013-02-10 14:54:09 -0500

    Added labeling and grid options.

--- graph_day.py
+++ graph_day.py
@@ -24,7 +24,7 @@ with conn:
 		sys.exit(1)
 
 	for row in rows:
-		data.append( (row[2], row[1][0:2]) )
+		data.append( (row[2], int(row[1][0:2])) )
 
 	print data
 
@@ -33,4 +33,10 @@ Y = [ x for (x,y) in data ]
 
 plt.plot( X, Y, ':rs' )
 plt.axis([0,24,0,300])
+plt.grid(True)
+plt.title("Glucose levels by time of day on " + date[0])
+plt.xlabel("Time of day (24HR Time)")
+plt.ylabel("Glucose Level")
+plt.xticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
+plt.yticks([20,40,60,80,100,120,140,160,180,200,220,240,260,280,300])
 plt.show()
