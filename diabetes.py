433f2090eb8c388a3c67110033d6b99e9acf6423
Brandon Powell <bpowell29a@gmail.com>
2013-02-12 13:24:11 -0500

    Added ability to display scatter plot.

--- diabetes.py
+++ diabetes.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+from graph_scatter import Graph_Scatter
 from graph_line import Graph_Line
 from import_db import import_glucose_csv
 
 import_glucose_csv()
 date = raw_input("Input date: ")
-g = Graph_Line()
-g.one_day(date)
+l = Graph_Line()
+l.one_day(date)
+s = Graph_Scatter()
+s.scatter()
