964f90883514e8bf346800fca1c61374e4067e98
Brandon Powell <bpowell29a@gmail.com>
2013-02-12 13:01:57 -0500

    Changed importing into the database a function.

--- import_db.py
+++ import_db.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 
-import sqlite3
 import csv
 
-conn = None
-conn = sqlite3.connect('database.db')
+from database import Database
 
-with conn:
-	cur = conn.cursor()
-	cur.execute('drop table if exists glucose')
-
-	cmd_create = "create table glucose(date text, time text, level integer, notes text)"
-	cur.execute(cmd_create)
+def import_glucose_csv():
+    db = Database()
+    db.execute('drop table if exists glucose')
+    db.execute('create table glucose(date text, time text, level integer, notes text)')
 
 	with open('glucose.csv', 'rb') as infile:
 		dr = csv.DictReader(infile, delimiter=',')
 		to_db = [(i['date'], i['time'], i['level'], i['notes']) for i in dr]
-	cur.executemany("insert into glucose (date, time, level, notes) values (?, ?, ?, ?);", to_db)
-	conn.commit()
-
+	db.executemany("insert into glucose (date, time, level, notes) values (?, ?, ?, ?);", to_db)
+    db.commit()
