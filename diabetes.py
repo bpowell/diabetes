df68f6e478b63636b95b4ea367a67915920cbc24
Brandon Powell <bpowell29a@gmail.com>
2013-02-12 13:06:14 -0500

    Main application now updates database every time it is ran.

--- diabetes.py
+++ diabetes.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 from graph_line import Graph_Line
+from import_db import import_glucose_csv
 
+import_glucose_csv()
 date = raw_input("Input date: ")
 g = Graph_Line()
 g.one_day(date)
