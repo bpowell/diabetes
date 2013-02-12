d8a9692d48eff8501adc3126115859c75d656e10
Brandon Powell <bpowell29a@gmail.com>
2013-02-12 13:05:53 -0500

    Properly formatted import_db.

--- import_db.py
+++ import_db.py
@@ -6,11 +6,14 @@ from database import Database
 
 def import_glucose_csv():
     db = Database()
+    db.open()
     db.execute('drop table if exists glucose')
     db.execute('create table glucose(date text, time text, level integer, notes text)')
 
-	with open('glucose.csv', 'rb') as infile:
-		dr = csv.DictReader(infile, delimiter=',')
-		to_db = [(i['date'], i['time'], i['level'], i['notes']) for i in dr]
-	db.executemany("insert into glucose (date, time, level, notes) values (?, ?, ?, ?);", to_db)
+    with open('glucose.csv', 'rb') as infile:
+        dr = csv.DictReader(infile, delimiter=',')
+        to_db = [(i['date'], i['time'], i['level'], i['notes']) for i in dr]
+
+    db.executemany("insert into glucose (date, time, level, notes) values (?, ?, ?, ?);", to_db)
     db.commit()
+    db.close()
