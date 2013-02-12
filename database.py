2b34677ff9423a7c0905cd29f507efcb24118069
Brandon Powell <bpowell29a@gmail.com>
2013-02-12 11:03:39 -0500

    Added database class to handle all database operations.

---
+++ database.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sqlite3
+import sys
+
+class Database:
+    connection = None
+    cursor = None
+
+    def open(self):
+        try:
+            self.connection = sqlite3.connect('database.db')
+            self.cursor = self.connection.cursor()
+        except:
+            print("Cannot open database. Please create database and try again. Now exiting.")
+            sys.exit(1)
+
+    def close(self):
+        self.connection.close()
+
+    def execute(self, command, data=None):
+        if data!=None:
+            self.cursor.execute(command, data)
+        else:
+            self.cursor.execute(command)
+
+    def select(self, command, data=None):
+        if data!=None:
+            self.cursor.execute(command, data)
+        else:
+            self.cursor.execute(command)
+
+        return self.cursor.fetchall()
