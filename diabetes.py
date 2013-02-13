b54132fb227aed3da643ca6ff1b1f236dc591bfe
Brandon Powell <bpowell29a@gmail.com>
2013-02-13 15:12:32 -0500

    Removed working test cases from main application.

--- diabetes.py
+++ diabetes.py
@@ -5,8 +5,9 @@ from graph_line import Graph_Line
 from import_db import import_glucose_csv
 
 import_glucose_csv()
-date = raw_input("Input date: ")
+#date = raw_input("Input date: ")
 l = Graph_Line()
-l.one_day(date)
+#l.one_day(date)
 s = Graph_Scatter()
-s.scatter()
+#s.scatter()
+s.scatter_morning()
