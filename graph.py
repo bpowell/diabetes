be450ef040763bc1e18814e6898c6c659e0bae44
Brandon Powell <bpowell29a@gmail.com>
2013-02-13 15:13:16 -0500

    Made an axis method to change the axis intervals and ticks.

--- graph.py
+++ graph.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 
 class Graph:
     def __init__(self):
-	plt.figure(num=None, figsize=(10, 8), dpi=100, facecolor='w', edgecolor='k')
-        plt.axis([0,24,0,300])
+        #plt.figure(num=None, figsize=(10, 8), dpi=100, facecolor='w', edgecolor='k')
         plt.grid(True)
         plt.xlabel("Time of Day (24HR Format)")
         plt.ylabel("Glucose Level")
@@ -14,10 +13,13 @@ class Graph:
     def title(self, t):
         plt.title(t)
 
-    def single_line(self, X, Y, line_type, xtick, ytick):
-        plt.xticks(xtick)
-        plt.yticks(ytick)
+    def single_line(self, X, Y, line_type):
         plt.plot(X, Y, line_type)
 
     def show(self):
         plt.show()
+
+    def axis(self, a, xtick, ytick):
+        plt.axis(a)
+        plt.xticks(xtick)
+        plt.yticks(ytick)
