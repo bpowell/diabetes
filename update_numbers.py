49afadfcddd838d7d6aaf56792a5f62d91b60b81
Brandon Powell <bpowell29a@gmail.com>
2014-06-14 22:35:28 -0400

    better input on numbers

--- update_numbers.py
+++ update_numbers.py
@@ -7,8 +7,20 @@ num_days = int(num_days)
 
 data = []
 
+date = ""
+
 for day in range(num_days):
-	date = raw_input("Enter date: ")
+        if date == "":
+            date = raw_input("Enter date: ")
+        else:
+            day = int(date[3:5])+1
+            if day<10:
+                day = "0" + str(day)
+            date = date[0:3]+str(day)+date[5:]
+            print(date+": correct date?")
+            correct = raw_input("Y/n: ")
+            if correct=="n":
+                date = raw_input("Enter date: ")
 
 	num_pokes = raw_input("Number of pokes: ")
 	num_pokes = int(num_pokes)
@@ -17,7 +29,7 @@ for day in range(num_days):
 		t = raw_input("Time: ")
 		g = raw_input("Number: ")
 		
-		data.append([date, t, g, None])
+                data.append([date, t[0:2]+':'+t[2:4], g, None])
 
 	print("\n")
 
